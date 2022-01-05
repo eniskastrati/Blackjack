7a77623f838807db8f0cf12b9d75f73d201fdffa
eniskastati <eniskastati18@gmail.com>
2022-01-05 15:32:57 +0100

    Layout change, Design and bit of functionality

--- app.js
+++ app.js
@@ -1,34 +1,54 @@
-let firstCard = 10
-let secondCard = 4
-let sum = firstCard + secondCard
-let hasBlackJack = false
-let isAlive = true
-let message = ""
-let messageEl = document.getElementById("message-el")
-let sumEl = document.getElementById("sum-el")
-let cardsEl = document.getElementById("cards-el")
+document.body.style.zoom="260%"
+let firstCard = randomIntFromInterval(2,11)
+let secondCard = randomIntFromInterval(2,9)
+let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+let message = "";
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
+let cardEl = document.getElementById("card-el");
 
+//function to restart the game
+function restart(){
+    location.reload(); 
+}
+
+//function to generate a ranodm number
+function randomIntFromInterval(min, max) { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+  
 function startGame() {
-    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
-    sumEl.textContent = "Sum: " + sum
+	cardsEl.textContent ="Cards: " +firstCard +" "+ secondCard;
+	sumEl.textContent = "Sum: " + sum;
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
     } else if (sum === 21) {
         message = "You've got Blackjack!"
-        hasBlackJack = true
-    } else {
+        alert("You've got Blackjack!")
+        hasBlackJack = true;
+    } else if(sum >= 22){
         message = "You're out of the game!"
-        isAlive = false
+		isAlive = false;
+        if(isAlive == false){
+            setTimeout(()=>restart(),4000);
+        }
     }
-    messageEl.textContent = message
+	messageEl.textContent = message;
 }
 
 
 function newCard() {
+    
     console.log("Drawing a new card from the deck!")
     // 1. Create a card variable, and hard code its value to a number (2-11)
-    
+	let nextCard = randomIntFromInterval(2,11)
     // 2. Add the new card to the sum variable
-    
-    // 3. Call startGame()
+    cardEl.textContent = "NewCard: "+ nextCard;
+	sum = sum + nextCard;
+	// 3. Call startGame()
+	startGame();
 }
+
